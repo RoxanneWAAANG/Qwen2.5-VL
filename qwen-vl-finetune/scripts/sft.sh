caea9fc3e4fcf3dfc58c8be0814949087f913bab
Ruoxin WANG <91651767+RoxanneWAAANG@users.noreply.github.com>
2025-06-10 21:52:09 -0400

    Update sft.sh

--- qwen-vl-finetune/scripts/sft.sh
+++ qwen-vl-finetune/scripts/sft.sh
@@ -18,14 +18,14 @@ deepspeed=./scripts/zero3_offload.json
 # Model configuration
 # ----------------------------
 # llm=Qwen/Qwen2.5-VL-7B-Instruct
-llm='/home/jack/Projects/yixin-llm/yixin-llm-data/MedicalGPT/weights/Qwen2.5-VL-3B-Instruct'
+llm='/home/jack/Projects/yixin-llm/yixin-llm-data/MedicalGPT/weights/Qwen2.5-VL-7B-Instruct'
 
 # ----------------------------
 # Training hyperparameters
 # ----------------------------
 lr=2e-5
 batch_size=4
-grad_accum_steps=4
+grad_accum_steps=16
 
 # ----------------------------
 # Training entry point
@@ -35,13 +35,16 @@ entry_file=./qwenvl/train/train_qwen.py
 # ----------------------------
 # Dataset config
 # ----------------------------
-datasets=healthgpt_reconstruction
+# datasets=healthgpt_reconstruction,healthgpt_superres,internet_segmentation,llava_rad_report_generation,llava_summarization,pmc_llama_qa,rate_ner,svlms_report_generation,ultrasam_segmentation
+# llava_rad_report_generation
+# llava_summarization -- exceed length (Token indices sequence length is longer than the specified maximum sequence length for this model (11885 > 8192). Running this sequence through the model will result in indexing errors)
+datasets=healthgpt_superres,pmc_llama_qa,rate_ner,internet_segmentation,svlms_report_generation,ultrasam_segmentation,llava_rad_report_generation
 
 # ----------------------------
 # Output config
 # ----------------------------
 run_name="qwen2vl-lora-baseline"
-output_dir=./output
+output_dir=/home/jack/Projects/yixin-llm/yixin-llm-data/MedicalGPT/weights/output_7b/
 logging_dir="${output_dir}/tensorboard_logs"
 
 # Training arguments
@@ -62,10 +65,10 @@ args="
     --per_device_eval_batch_size $((batch_size*2)) \
     --gradient_accumulation_steps ${grad_accum_steps} \
     --max_pixels 50176 \
-    --min_pixels 784 \
+    --min_pixels 512 \
     --eval_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 400 \
+    --save_steps 300 \
     --save_total_limit 1 \
     --learning_rate ${lr} \
     --weight_decay 0 \
@@ -75,7 +78,7 @@ args="
     --logging_steps 1 \
     --model_max_length 8192 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 1 \
     --run_name ${run_name} \
     "
 
