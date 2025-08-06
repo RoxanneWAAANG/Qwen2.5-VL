fcefee92b0c7bc0d5528492a37e5c59e16a3ce4d
Ruoxin WANG <91651767+RoxanneWAAANG@users.noreply.github.com>
2025-08-06 11:32:00 -0400

    organize

--- qwen-vl-finetune/qwenvl/data/__init__.py
+++ qwen-vl-finetune/qwenvl/data/__init__.py
@@ -1,92 +1,26 @@
 import re
 
 # Define placeholders for dataset paths
-HEALTHGPT_RECONSTRUCTION = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/healthgpt_reconstruct_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
-}
-
-HEALTHGPT_SUPERRES = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/healthgpt_superres_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
-}
-
-INTERNET_SEGMENTATION = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/internet_seg_dataset.jsonl",
-    "data_path": "/home/jack/.cache/kagglehub/datasets/deathtrooper/multichannel-glaucoma-benchmark-dataset/versions/10/full-fundus/full-fundus",
-}
-
-LLAVA_RAD_REPORT_GENERATION = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/llava_rad_rg_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
-}
-
-LLAVA_SUMMARIZATION = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/llava_sum_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/dummy_images",
-}
-
-PMC_LLAMA_QA = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/pmc_llama_medqa_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/dummy_images",
-}
-
-RATE_NER = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/rate_ner_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/dummy_images",
-}
-
-SVLMS_REPORT_GENERATION = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/svlms_fundus_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/deepeyenet/deepeyenet/eyenet0420/train_set",
-}
-
-ULTRASAM_SEGMENTATION = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/ultrasam_seg_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/UltraSam/dataset/BrEaST/BrEaST-Lesions_USG-images_and_masks-Dec-15-2023/images",
-}
-
-UNIGRADICON_REG = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/unigradicon_reg_dataset.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
-}
-
 SINGLE_TOOL_MULTIROUND = {
     "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/multi_round/single_tool_multiround.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
+    "data_path": "",
 }
 
 MULTI_TOOL_MULTIROUND = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/multi_round/multi_tool_multiround.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
+    "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/multi_round/multi_tool_multiround_new.jsonl",
+    "data_path": "",
 }
 
 MULTI_TOOL_SINGLE_ROUND = {
     "annotation_path": "/home/jack/Projects/yixin-llm/Qwen2.5-VL/qwen-vl-finetune/build_dataset/multi_round/multi_tool_single_round.jsonl",
-    "data_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/instruct_dataset/mimic-cxr-5k/5k",
-}
-
-PATH_2000 = {
-    "annotation_path": "/home/jack/Projects/yixin-llm/yixin-llm-data/yptests/Qwen2.5-VL/qwen-vl-finetune/build_dataset/tool_instruct/instruction_2000_img_updated.json",
-    "data_path": "" ,
+    "data_path": "",
 }
 
 
 data_dict = {
-    "healthgpt_reconstruction": HEALTHGPT_RECONSTRUCTION,
-    "healthgpt_superres": HEALTHGPT_SUPERRES,
-    "internet_segmentation": INTERNET_SEGMENTATION,
-    "llava_rad_report_generation": LLAVA_RAD_REPORT_GENERATION,
-    "llava_summarization": LLAVA_SUMMARIZATION,
-    "pmc_llama_qa": PMC_LLAMA_QA,
-    "rate_ner": RATE_NER,
-    "svlms_report_generation": SVLMS_REPORT_GENERATION,
-    "ultrasam_segmentation": ULTRASAM_SEGMENTATION,
-    "unigradicon_reg": UNIGRADICON_REG,
     "single_tool_multi_round": SINGLE_TOOL_MULTIROUND,
     "multi_tool_multi_round": MULTI_TOOL_MULTIROUND,
     "multi_tool_single_round": MULTI_TOOL_SINGLE_ROUND,
-    "path_2000": PATH_2000,
 }
 
 
